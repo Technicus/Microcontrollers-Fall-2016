6af9454dcab964191bbc507bb4ad9018d1db95ac
Gardner <FGARDNER@madisoncollege.edu>
2016-09-08 15:57:27 -0500

    Interrupts

--- Code-Design/Code/Library/Interrupts.h
+++ Code-Design/Code/Library/Interrupts.h
@@ -9,38 +9,86 @@
 #define INTERRUPT_2 #INT_EXT2 // PIN_B0 external interrupt circuit
 
 struct INTCON_{
-	int RBIF:1;
-	int INT0IF:1;
-	int TMR0IF:1;
-	int RBIE:1;
-	int INT0IE:1;
-	int TMR0IE:1;
-	int PEIE:1;
-	int GIE:1;
+	int RBIF:1;     // RBIF: RB Port Change Interrupt Flag bit
+					//   1 = At least one of the RB<7:4> pins changed state (must be cleared in software)
+					//	 0 = None of the RB<7:4> pins have changed state
+	int INT0IF:1;   // INT0IF: INT0 External Interrupt Flag bit
+					//   1 = The INT0 external interrupt occurred (must be cleared in software)
+					//	 0 = The INT0 external interrupt did not occur 
+	int TMR0IF:1;   // TMR0IF: TMR0 Overflow Interrupt Flag bit
+					//   1 = TMR0 register has overflowed (must be cleared in software)
+					//	 0 = TMR0 register did not overflow
+	int RBIE:1;     // RBIE: RB Port Change Interrupt Enable bit
+					//   1 = Enables the RB port change interrupt
+					//	 0 = Disables the RB port change interrupt
+	int INT0IE:1;   // INT0IE: INT0 External Interrupt Enable bit *** needed to have interrupt 
+					//   1 = Enables the INT0 external interrupt
+					//   0 = Disables the INT0 external interrupt                            
+	int TMR0IE:1;   // TMR0IE: TMR0 Overflow Interrupt Enable bit
+					//   1 = Enables the TMR0 overflow interrupt
+					//	 0 = Disables the TMR0 overflow interrupt
+	int PEIE:1;     // PEIE/GIEL: Peripheral Interrupt Enable bit.  Localized enable bit for INT0. *** needed to have interrupt
+					//	 When IPEN = 0:
+					//	   1 = Enables all unmasked peripheral interrupts
+					//	   0 = Disables all peripheral interrupts
+					//	 When IPEN = 1:
+					//	   1 = Enables all low-priority peripheral interrupts
+					//	   0 = Disables all low-priority peripheral interrupts
+	int GIE:1;      // GIE/GIEH:: Global Interrupt Enable bit.  Master switch for all interrupts.  *** needed to have interrupt
+					//   When IPEN = 0:
+					//	   1 = Enables all unmasked interrupts
+					//	   0 = Disables all interrupts
+					//	 When IPEN = 1:
+					//	   1 = Enables all high-priority interrupts
+					//	   0 = Disables all interrupts
 	};
 struct INTCON_ *INTCON = REGISTER_INTCON;
 
 struct INTCON_2{
-	int RBIP:1;
-	int UNIMPLEMENTED_0:1;
-	int TMROIP:1;
-	int UNIMPLEMENTED_1:1;
-	int INTEDG2:1;
-	int INTEDG1:1;
-	int INTEDG0:1;
-	int RBPU:1;
+	int RBIP:1;             // RBIP: RB Port Change Interrupt Priority bit
+							//	 1 = High priority
+							//	 0 = Low priority
+	int UNIMPLEMENTED_0:1;	// Read as ‘0’
+	int TMROIP:1;           // TMR0 Overflow Interrupt Priority bit
+							//	 1 = High priority
+							//	 0 = Low priority
+	int UNIMPLEMENTED_1:1;	// Read as ‘0’
+	int INTEDG2:1;          // External Interrupt 2 Edge Select bit
+							//   1 = Interrupt on rising edge
+							//   0 = Interrupt on falling edge 
+	int INTEDG1:1;          // External Interrupt 1 Edge Select bit
+							//   1 = Interrupt on rising edge
+							//   0 = Interrupt on falling edge
+	int INTEDG0:1;          // External Interrupt 0 Edge Select bit
+							//   1 = Interrupt on rising edge
+							//   0 = Interrupt on falling edge
+	int RBPU:1;             // PORTB Pull-up Enable bit
+							//	 1 = All PORTB pull-ups are disabled
+							//	 0 = PORTB pull-ups are enabled by individual port latch values
 	};
 struct INTCON_2 *INTCON2 = REGISTER_INTCON2;
 
 struct INTCON_3{
-	int INT1F:1;
-	int INT2F:1;
-	int UNIMPLEMENTED_0:1;
-	int INT1E:1;
-	int INT2E:1;
-	int UNIMPLEMENTED_1:1;
-	int INT1P:1;
-	int INT2P:1;
+	int INT1F:1;             // INT1IF: INT1 External Interrupt Flag bit
+							 //	  1 = The INT1 external interrupt occurred (must be cleared in software)
+							 //	  0 = The INT1 external interrupt did not occur
+	int INT2F:1;			 // INT2IF: INT2 External Interrupt Flag bit
+							 //	  1 = The INT2 external interrupt occurred (must be cleared in software)
+							 //	  0 = The INT2 external interrupt did not occur
+	int UNIMPLEMENTED_0:1;   // Read as ‘0’
+	int INT1E:1;             // INT1IE: INT1 External Interrupt Enable bit
+							 //	  1 = Enables the INT1 external interrupt
+							 //	  0 = Disables the INT1 external interrupt
+	int INT2E:1;             // INT2IE: INT2 External Interrupt Enable bit
+							 //	  1 = Enables the INT2 external interrupt
+							 //	  0 = Disables the INT2 external interrupt
+	int UNIMPLEMENTED_1:1;   // Read as ‘0’
+	int INT1P:1;			 // INT1IP: INT1 External Interrupt Priority bit
+							 //	  1 = High priority
+							 //	  0 = Low priority
+	int INT2P:1;			 // INT2IP: INT2 External Interrupt Priority bit
+							 //	  1 = High priority
+							 //	  0 = Low priority
 	};
 struct INTCON_3 *INTCON3 = REGISTER_INTCON3;
 
--- Code-Design/Code/Projects/GPIO/PORTC_LED_Button_Interrupt_External.C
+++ Code-Design/Code/Projects/GPIO/PORTC_LED_Button_Interrupt_External.C
@@ -1,12 +1,24 @@
-// Blink Test
+// Interrupt example
+
+// Master, Local, Individual switch to turn on interrupt
+
 #included <18f4520.h>
 #use delay (clock = 20000000)
 #fuses HS, NOWDT, NOLVP
 #include "../Library/18F4520ptr.h"
 
+//int x = 255;
+
 #INT_EXT0 // PIN_B0 external interrupt circuit
 void int_ext_isr(){
-	*PORT^=0X80;
+	INTCON -> INT0IE = 0;  // Software debouncing
+	//*PORT^=0X80;
+	if (&x == 255){
+		&x = 100;
+	} else {
+		&x = 255;
+	}
+	INTCON -> INT0IE = 1;  // Software debouncing
 }
 
 #INT_EXT1 // PIN_B1 external interrupt circuit
@@ -17,15 +29,19 @@ void int_ext_isr(){
 void int_ext_isr(){
 }
 
-main(){
-	*ADCON1 = 0x0f;
-	*TRISB = 0x01; // B0 Input
-	INTCON -> GIE = 1;
-	INTCON -> PEIE = 1;
-	INTCON -> INT0IE = 1;
+main//(){
+	int x = 255;
+
+	ADCON1 -> PCFGX=0xF; // *ADCON1 = 0x0f; // Select as digital
+	*TRISB = 0x01  //TRISB = 0x01; // B0 Input
+
+	INTCON -> GIE = 1; 			// Global Interrupt Enable bit
+	INTCON -> PEIE = 1; 		// Peripheral Interrupt Enable bit
+	INTCON -> INT0IE = 1;		// INT0 External Interrupt Enable bit
+	INTCON2 -> INTEDG0 = 1;		// External Interrupt 0 Edge Select bit
 	
 	while(1){
-		*PORTB ^= 0x40;
-		//delay_ms(300);
+		*PORTB ^= 0x80;
+		delay_ms(x);
 		}
 }
