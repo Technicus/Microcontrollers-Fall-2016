57d8378a354c21c7c307b8c38dd9c3ecf3a0994d
Gardner <FGARDNER@madisoncollege.edu>
2016-09-28 16:42:42 -0500

    ?

--- Code-Design/Code/Projects/LCD/lcd.c
+++ Code-Design/Code/Projects/LCD/lcd.c
@@ -1,38 +1,30 @@
-/* Initial lcd test
-   2015-.09.24
-   ../../designs/LCDpanel_1.design
+/*
+ * Initial lcd test
+ * 2016.09.28
 */
 
-#include <18f4520.h>
-#use delay (clock = 200000000)
-#fuses HS, NOWDT, NOLvP
-#include "../Library/18f4520ptr.h"
-#include "../library/modifiedlcd.h"
+#include "../../Library/Library_18f4520.h"
 
-float Vres = 5.0 / 1023.0;
+//float Vres = 5.0 / 1023.0;
 
 main(){
+   char press = '@';
+   int X = 0;
 
    lcd_init();
-   //printf(lcd_putc, "\fHello \n:)");
-
-   ADCON0->ADON = 1;
-   ADCON0->CHSx = 1;
-   ADCON1->PCFGX = 8;
-
-   *TRISA = 0x02;  // set A0 as input
-
-   ADCON2->ADFM = 1; // right justify
+   printf(lcd_putc, "\fHello \n:)");
 
    while(1){
 
-      ADCON0->GODONE = 1; // go
-      while(ADCON0->GODONE){}
+	//printf(lcd_putc, "\n:) X=%lu",(long)X++);
 
-      printf(lcd_putc, "\f lcd");
-      printf(lcd_putc, "\n %f", *Q * Vres);
+	press = keyPress();
+      if( press != '@'){
+         printf(lcd_putc, "\f %c", press);
+      }
 
-      delay_ms(300);
+	delay_ms(10);
 
-   };
+   }
+
 }
